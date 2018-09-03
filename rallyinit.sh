9f5a64a7dafbc90d2cdf7dace63d0f00b75e9b59
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2018-09-03 04:59:34 +0300

    various improvements

--- rallyinit.sh
+++ rallyinit.sh
@@ -1,5 +1,3 @@
-rally-manage db recreate
+rally db recreate
 rally deployment create --fromenv --name snap_generated
-rally verify install --system --source /snap/fcbtest/current/tempest 
-pip2 install /snap/fcbtest/current/testtools-2.3.0-py2.py3-none-any.whl --no-deps
-pip install /snap/fcbtest/current/extras-1.0.0-py2.py3-none-any.whl --no-deps
+#rally verify install --system --source /snap/fcbtest/current/tempest 
