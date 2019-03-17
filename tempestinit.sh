e2888738d65dea1f24b749f8b6d64152a80d1b10
Dmitrii Shcherbakov <dmitrii.shcherbakov@canonical.com>
2019-03-17 20:28:14 +0300

    Various changes
    * use a rally patch instead of a rally-openstack patch per a review
    discussion upstream for context comparison implementation;
    * omit extra removal file removal steps (this doesn't seem to cause
    problems anymore with the latest snapcraft on clean builds);
    * do not use a specific tag for a tempest repo in rallyinit.sh
    (otherwise a patch for keystone v3 awareness is not cloned to the target
    repo used by the tempest verifier).

--- tempestinit.sh
+++ tempestinit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rally verify create-verifier --type "tempest" --source /snap/fcbtest/current/tempest --version "19.0.0" --system-wide --name tempestverifier
+rally verify create-verifier --type "tempest" --source /snap/fcbtest/current/tempest --system-wide --name tempestverifier
